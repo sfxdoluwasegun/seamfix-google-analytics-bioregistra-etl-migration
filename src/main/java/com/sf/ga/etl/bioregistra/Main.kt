975db2c1f7f0e1a84468d2389636b8d8edf38b0b
nonsobiose <cbiose@seamfix.com>
2019-07-10 15:15:45 +0100

    - Minor fix #DSC-269

--- src/main/java/com/sf/ga/etl/bioregistra/Main.kt
+++ src/main/java/com/sf/ga/etl/bioregistra/Main.kt
@@ -1,10 +1,13 @@
 package com.sf.ga.etl.bioregistra
 
+import java.io.File
 import java.net.SocketException
 import java.net.UnknownHostException
 
 
 fun main() {
+
+
     val googleAnalytics = GoogleAnalytics()
     val reporter = googleAnalytics.initialiseAnalytics("/Users/nonsobiose/Downloads/ga-bioregistra-etl-fbc2fa24d6cb.json", "/Users/nonsobiose/IdeaProjects/Google Analytics ETL Bioregistra/Query.json")
     val queries = googleAnalytics.buildQuery()
